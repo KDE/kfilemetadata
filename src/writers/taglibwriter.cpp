982ebe58b3c88ea5bb10e675dc6729e1e43b7b39
Alexander Stippich <a.stippich@gmx.net>
2019-04-02 20:34:26 +0200

    Rewrite taglib writer to use property interface
    Summary:
    Rewrite the taglib writer to use taglib's
    unified property interface, which is more extensible.
    
    Test Plan: tests pass
    
    Reviewers: bruns, mgallien, broulik, cfeck
    
    Reviewed By: bruns
    
    Subscribers: kde-frameworks-devel, #baloo
    
    Tags: #frameworks, #baloo
    
    Differential Revision: https://phabricator.kde.org/D18601

--- src/writers/taglibwriter.cpp
+++ src/writers/taglibwriter.cpp
@@ -21,16 +21,26 @@
 #include "taglibwriter.h"
 
 #include <taglib.h>
+#include <tfilestream.h>
+#include <tpropertymap.h>
 #include <tstring.h>
-#include <id3v2tag.h>
-#include <fileref.h>
+#include <aifffile.h>
+#include <apefile.h>
+#include <asffile.h>
+#include <flacfile.h>
+#include <mp4file.h>
+#include <mpcfile.h>
+#include <mpegfile.h>
+#include <oggfile.h>
+#include <opusfile.h>
+#include <vorbisfile.h>
+#include <speexfile.h>
+#include <wavpackfile.h>
+#include <wavfile.h>
 
-using namespace KFileMetaData;
+#include <QDebug>
 
-TagLibWriter::TagLibWriter(QObject* parent)
-    : WriterPlugin(parent)
-{
-}
+namespace {
 
 const QStringList supportedMimeTypes = {
     QStringLiteral("audio/flac"),
@@ -47,11 +57,59 @@ const QStringList supportedMimeTypes = {
     QStringLiteral("audio/x-ms-wma"),
     QStringLiteral("audio/x-musepack"),
     QStringLiteral("audio/x-opus+ogg"),
-    QStringLiteral("audio/x-speex"),
+    QStringLiteral("audio/x-speex+ogg"),
     QStringLiteral("audio/x-vorbis+ogg"),
     QStringLiteral("audio/x-wav"),
     QStringLiteral("audio/x-wavpack"),
 };
+
+}
+
+using namespace KFileMetaData;
+
+void writeGenericProperties(TagLib::PropertyMap &oldProperties, const PropertyMap &newProperties)
+{
+    if (newProperties.contains(Property::Title)) {
+        oldProperties.replace("TITLE", QStringToTString(newProperties.value(Property::Title).toString()));
+    }
+
+    if (newProperties.contains(Property::Artist)) {
+        oldProperties.replace("ARTIST", QStringToTString(newProperties.value(Property::Artist).toString()));
+    }
+
+    if (newProperties.contains(Property::Album)) {
+        oldProperties.replace("ALBUM", QStringToTString(newProperties.value(Property::Album).toString()));
+    }
+
+    if (newProperties.contains(Property::TrackNumber)) {
+        int trackNumber = newProperties.value(Property::TrackNumber).toInt();
+        //taglib requires uint
+        if (trackNumber >= 0) {
+            oldProperties.replace("TRACKNUMBER", QStringToTString(newProperties.value(Property::TrackNumber).toString()));
+        }
+    }
+
+    if (newProperties.contains(Property::ReleaseYear)) {
+        int year = newProperties.value(Property::ReleaseYear).toInt();
+        //taglib requires uint
+        if (year >= 0) {
+            oldProperties.replace("DATE", QStringToTString(newProperties.value(Property::ReleaseYear).toString()));
+        }
+    }
+
+    if (newProperties.contains(Property::Genre)) {
+        oldProperties.replace("GENRE", QStringToTString(newProperties.value(Property::Genre).toString()));
+    }
+
+    if (newProperties.contains(Property::Comment)) {
+        oldProperties.replace("COMMENT", QStringToTString(newProperties.value(Property::Comment).toString()));
+    }
+}
+
+TagLibWriter::TagLibWriter(QObject* parent)
+    : WriterPlugin(parent)
+{
+}
 
 QStringList TagLibWriter::writeMimetypes() const
 {
@@ -62,61 +120,110 @@ void TagLibWriter::write(const WriteData& data)
 {
     const QString fileUrl = data.inputUrl();
     const PropertyMap properties = data.getAllProperties();
+    const QString mimeType = data.inputMimetype();
 
-    TagLib::FileRef file(fileUrl.toUtf8().constData(), true);
-    if (file.isNull()) {
+    TagLib::FileStream stream(fileUrl.toUtf8().constData(), false);
+    if (!stream.isOpen()) {
+        qWarning() << "Unable to open file in write mode: " << fileUrl;
         return;
     }
 
-    TagLib::Tag* tags = file.tag();
-
-    TagLib::String title;
-    TagLib::String artist;
-    TagLib::String album;
-    TagLib::String genre;
-    TagLib::String comment;
-
-    if (properties.contains(Property::Title)) {
-        title = QStringToTString(properties.value(Property::Title).toString());
-        tags->setTitle(title);
-    }
-
-    if (properties.contains(Property::Artist)) {
-        artist = QStringToTString(properties.value(Property::Artist).toString());
-        tags->setArtist(artist);
-    }
-
-    if (properties.contains(Property::Album)) {
-        album = QStringToTString(properties.value(Property::Album).toString());
-        tags->setAlbum(album);
-    }
-
-    if (properties.contains(Property::TrackNumber)) {
-        int trackNumber = properties.value(Property::TrackNumber).toInt();
-        //taglib requires uint
-        if (trackNumber >= 0) {
-            tags->setTrack(trackNumber);
+    if ((mimeType == QLatin1String("audio/mpeg")) || (mimeType == QLatin1String("audio/mpeg3"))
+            || (mimeType == QLatin1String("audio/x-mpeg"))) {
+        TagLib::MPEG::File file(&stream, TagLib::ID3v2::FrameFactory::instance(), false);
+        if (file.isValid()) {
+            auto savedProperties = file.properties();
+            writeGenericProperties(savedProperties, properties);
+            file.setProperties(savedProperties);
+            file.save();
+        }
+    } else if (mimeType == QLatin1String("audio/x-aiff")) {
+        TagLib::RIFF::AIFF::File file(&stream, false);
+        if (file.isValid()) {
+            auto savedProperties = file.properties();
+            writeGenericProperties(savedProperties, properties);
+            file.setProperties(savedProperties);
+            file.save();
+        }
+    } else if ((mimeType == QLatin1String("audio/wav")) || (mimeType == QLatin1String("audio/x-wav"))) {
+        TagLib::RIFF::WAV::File file(&stream, false);
+        if (file.isValid()) {
+            auto savedProperties = file.properties();
+            writeGenericProperties(savedProperties, properties);
+            file.setProperties(savedProperties);
+            file.save();
+        }
+    } else if (mimeType == QLatin1String("audio/x-musepack")) {
+        TagLib::MPC::File file(&stream, false);
+        if (file.isValid()) {
+            auto savedProperties = file.properties();
+            writeGenericProperties(savedProperties, properties);
+            file.setProperties(savedProperties);
+            file.save();
+        }
+    } else if (mimeType == QLatin1String("audio/x-ape")) {
+        TagLib::APE::File file(&stream, false);
+        if (file.isValid()) {
+            auto savedProperties = file.properties();
+            writeGenericProperties(savedProperties, properties);
+            file.setProperties(savedProperties);
+            file.save();
+        }
+    } else if (mimeType == QLatin1String("audio/x-wavpack")) {
+        TagLib::WavPack::File file(&stream, false);
+        if (file.isValid()) {
+            auto savedProperties = file.properties();
+            writeGenericProperties(savedProperties, properties);
+            file.setProperties(savedProperties);
+            file.save();
+        }
+    } else if (mimeType == QLatin1String("audio/mp4")) {
+        TagLib::MP4::File file(&stream, false);
+        if (file.isValid()) {
+            auto savedProperties = file.properties();
+            writeGenericProperties(savedProperties, properties);
+            file.setProperties(savedProperties);
+            file.save();
+        }
+    } else if (mimeType == QLatin1String("audio/flac")) {
+        TagLib::FLAC::File file(&stream, TagLib::ID3v2::FrameFactory::instance(), false);
+        if (file.isValid()) {
+            auto savedProperties = file.properties();
+            writeGenericProperties(savedProperties, properties);
+            file.setProperties(savedProperties);
+            file.save();
+        }
+    } else if (mimeType == QLatin1String("audio/ogg") || mimeType == QLatin1String("audio/x-vorbis+ogg")) {
+        TagLib::Ogg::Vorbis::File file(&stream, false);
+        if (file.isValid()) {
+            auto savedProperties = file.properties();
+            writeGenericProperties(savedProperties, properties);
+            file.setProperties(savedProperties);
+            file.save();
+        }
+    } else if (mimeType == QLatin1String("audio/opus") || mimeType == QLatin1String("audio/x-opus+ogg")) {
+        TagLib::Ogg::Opus::File file(&stream, false);
+        if (file.isValid()) {
+            auto savedProperties = file.properties();
+            writeGenericProperties(savedProperties, properties);
+            file.setProperties(savedProperties);
+            file.save();
+        }
+    } else if (mimeType == QLatin1String("audio/speex") || mimeType == QLatin1String("audio/x-speex+ogg")) {
+        TagLib::Ogg::Speex::File file(&stream, false);
+        if (file.isValid()) {
+            auto savedProperties = file.properties();
+            writeGenericProperties(savedProperties, properties);
+            file.setProperties(savedProperties);
+            file.save();
+        }
+    } else if (mimeType == QLatin1String("audio/x-ms-wma")) {
+        TagLib::ASF::File file(&stream, false);
+        if (file.isValid()) {
+            auto savedProperties = file.properties();
+            writeGenericProperties(savedProperties, properties);
+            file.setProperties(savedProperties);
+            file.save();
         }
     }
-
-    if (properties.contains(Property::ReleaseYear)) {
-        int year = properties.value(Property::ReleaseYear).toInt();
-        //taglib requires uint
-        if (year >= 0) {
-            tags->setYear(year);
-        }
-    }
-
-    if (properties.contains(Property::Genre)) {
-        genre = QStringToTString(properties.value(Property::Genre).toString());
-        tags->setGenre(genre);
-    }
-
-    if (properties.contains(Property::Comment)) {
-        comment = QStringToTString(properties.value(Property::Comment).toString());
-        tags->setComment(comment);
-    }
-
-
-    file.save();
 }
