db2e9955a715dc36d5b20729a64434c2088f2e67
Kai Uwe Broulik <kde@privat.broulik.de>
2022-10-11 18:43:16 +0200

    FFmpegExtractor: Extract creation time

--- src/extractors/ffmpegextractor.cpp
+++ src/extractors/ffmpegextractor.cpp
@@ -253,6 +253,14 @@ void FFmpegExtractor::extract(ExtractionResult* result)
             int year = QString::fromUtf8(entry->value).toInt();
             result->add(Property::ReleaseYear, year);
         }
+
+        entry = av_dict_get(dict, "creation_time", nullptr, 0);
+        if (entry) {
+            const QDateTime date = QDateTime::fromString(QString::fromUtf8(entry->value), Qt::ISODate);
+            if (date.isValid()) {
+                result->add(Property::CreationDate, date);
+            }
+        }
     }
 }
 
