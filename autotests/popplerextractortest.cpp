f9f483e69ecab171269a1ad08c13d30f765be9d0
Stefan Brüns <stefan.bruens@rwth-aachen.de>
2024-03-17 22:52:58 +0100

    [PopplerExtractorTest] Verify multicolumn PDF content (currently broken)
    The PDF content extraction currently uses a text "layout" (see
    poppler `pdftotext -layout ...`) when extracting the content, i.e.
    the lines of multiple columns will be interspersed.
    
    Add a PDF file which uses multiple columns and contains the required
    structures to recreate the correct text flow.
    
    Unfortunately, there is no simple way to fix this, as the
    `RawOrderLayout` of `Popper::Page::text(...)` creates even worse
    output than the default `PhysicalLayout`, (missing spaces between words,
    or no output at all).
    
    Also add the corresponding ODT source document.

--- autotests/popplerextractortest.cpp
+++ autotests/popplerextractortest.cpp
@@ -76,6 +76,7 @@ void PopplerExtractorTest::testExtraction()
     }
     QCOMPARE(result.properties().size(), expectedProperties.size());
 
+    QEXPECT_FAIL("Multicol - metadata and text", "Content extracted uses layout", Continue);
     QCOMPARE(result.text(), content);
 }
 
@@ -101,6 +102,20 @@ void PopplerExtractorTest::testExtraction_data()
     QTest::addRow("No extraction")     << QStringLiteral("test.pdf") << ER::Flags{ER::ExtractNothing}  << QString() << KFileMetaData::PropertyMultiMap{};
     QTest::addRow("Metadata only")     << QStringLiteral("test.pdf") << ER::Flags{ER::ExtractMetaData} << QString() << simplePdfProperties;
     QTest::addRow("Metadata and text") << QStringLiteral("test.pdf") << ER::Flags{ER::ExtractMetaData | ER::ExtractPlainText} << simplePdfContent << simplePdfProperties;
+
+    KFileMetaData::PropertyMultiMap multicolPdfProperties{
+        {Property::Author, QStringLiteral("Author KFM")},
+        {Property::Title, QStringLiteral("KFileMetaData Title")},
+        {Property::Subject, QStringLiteral("KFileMetaData Subject")},
+        {Property::Generator, QStringLiteral("LibreOffice 7.6")},
+        {Property::PageCount, 1},
+        {Property::CreationDate, QDateTime::fromString(QStringLiteral("2024-03-18T01:37:42+00"), Qt::ISODate)},
+    };
+    auto multicolPdfContent = QStringLiteral("1: First headline for KfileMetaData\n");
+
+    QTest::addRow("Multicol - no extraction")     << QStringLiteral("test_multicolumn.pdf") << ER::Flags{ER::ExtractNothing}  << QString() << KFileMetaData::PropertyMultiMap{};
+    QTest::addRow("Multicol - metadata only")     << QStringLiteral("test_multicolumn.pdf") << ER::Flags{ER::ExtractMetaData} << QString() << multicolPdfProperties;
+    QTest::addRow("Multicol - metadata and text") << QStringLiteral("test_multicolumn.pdf") << ER::Flags{ER::ExtractMetaData | ER::ExtractPlainText} << multicolPdfContent << multicolPdfProperties;
 }
 
 QTEST_GUILESS_MAIN(PopplerExtractorTest)
