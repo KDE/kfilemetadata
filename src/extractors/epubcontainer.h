1dc8b57ce48317c2ba331cbb29a17a13e68c5688
Carl Schwan <carl@carlschwan.eu>
2025-04-29 19:13:21 +0200

    Replace libepub by Qt based solution
    The original implementation for this epub parser is from
    github.com/sandsmark/epubreader. It is currently used in production in
    Arianna. The goal is to reuse to reuse KFileMetadata in Arianna as
    abstraction over different book formats.

---
+++ src/extractors/epubcontainer.h
@@ -0,0 +1,121 @@
+// SPDX-FileCopyrightText: 2018 Martin T. H. Sandsmark <martin.sandsmark@kde.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+#pragma once
+
+#include <QMimeDatabase>
+#include <QDomNode>
+#include <QHash>
+#include <QList>
+#include <QObject>
+#include <QSet>
+#include <KZip>
+
+class KArchiveDirectory;
+class KArchiveFile;
+class QXmlStreamReader;
+
+struct EpubItem {
+    QString path;
+    QByteArray mimetype;
+};
+
+struct EpubPageReference {
+    enum StandardType {
+        CoverPage,
+        TitlePage,
+        TableOfContents,
+        Index,
+        Glossary,
+        Acknowledgements,
+        Bibliography,
+        Colophon,
+        CopyrightPage,
+        Dedication,
+        Epigraph,
+        Foreword,
+        ListOfIllustrations,
+        ListOfTables,
+        Notes,
+        Preface,
+        Text,
+        Other
+    };
+
+    static StandardType typeFromString(const QStringView &name);
+
+    QString target;
+    QString title;
+};
+
+struct Collection {
+    enum Type {
+        Set,
+        Series,
+        Unknow,
+    };
+
+    QString name;
+    Type type;
+    size_t position;
+};
+
+class EPubContainer
+{
+public:
+    explicit EPubContainer() {}
+
+    bool openFile(const QString &path);
+
+    EpubItem epubItem(const QString &id) const
+    {
+        return m_items.value(id);
+    }
+
+    QSharedPointer<QIODevice> ioDevice(const QString &path);
+    QImage image(const QString &id);
+    QList<Collection> collections() const;
+    QStringList metadata(const QStringView &key) const;
+    QHash<QString, QStringList> metadata() const {
+        return m_metadata;
+    }
+
+    QStringList items() const
+    {
+        return m_orderedItems;
+    }
+
+    QString standardPage(EpubPageReference::StandardType type) const
+    {
+        return m_standardReferences.value(type).target;
+    }
+
+    QString errorString() const;
+
+private:
+    bool parseMimetype();
+    bool parseContainer();
+    bool parseContentFile(const QString &filepath);
+    bool parseMetadataItem(const QDomNode &metadataNode, const QDomNodeList &nodeList);
+    bool parseMetadataPropertyItem(const QDomElement &metadataElemenent, const QDomNodeList &nodeList);
+    bool parseManifestItem(const QDomNode &manifestNodes, const QString &currentFolder);
+    bool parseSpineItem(const QDomNode &spineNode);
+    bool parseGuideItem(const QDomNode &guideItem);
+
+    const KArchiveFile *file(const QString &path);
+
+    std::unique_ptr<KZip> m_archive;
+    const KArchiveDirectory *m_rootFolder = nullptr;
+
+    QHash<QString, QStringList> m_metadata;
+    QList<Collection> m_collections;
+
+    QHash<QString, EpubItem> m_items;
+    QStringList m_orderedItems;
+    QSet<QString> m_unorderedItems;
+
+    QHash<EpubPageReference::StandardType, EpubPageReference> m_standardReferences;
+    QHash<QString, EpubPageReference> m_otherReferences;
+    QMimeDatabase m_mimeDatabase;
+    QString m_errorString;
+};
