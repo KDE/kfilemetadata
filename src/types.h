KDE/kfilemetadata

commit 3224dd414ee8aafe6b3666dfc811044583f0c23f

Port API documentation to QDoc syntax

src/types.h
@@ -10,83 +10,63 @@
 
 #include <QVariant>
 
-/**
+/*!
+ * \namespace KFileMetaData
+ * \inheaderfile KFileMetaData/Types
+ * \inmodule KFileMetaData
  * The KFileMetaData namespace
  */
-namespace KFileMetaData {
+namespace KFileMetaData
+{
 
-/**
+/*!
+ * \namespace KFileMetaData::Type
+ * \inmodule KFileMetaData
  * The Type namespace
  */
-namespace Type {
+namespace Type
+{
 
-/**
+/*!
  * A Type represents a way to represent a way to group files based on
  * a higher level view, which the user generally expects.
  *
  * Every extractor provides a list of types applicable for each file.
+ *
+ * \omitvalue FirstType
+ * \value Empty
+ * \value Archive A file which contains a compressed collection of other files eg - tar, zip, rar, gz
+ *  File types which use e.g. zip as an implementation detail -
+ *  Java JAR files, OpenDocument formats, etc. - are not treated as Archive
+ * \value Audio Used to mark any file which just contains audio. Do not use this type if the file also contains Video
+ * \value Video Any file which contains Video. It may also contain Audio
+ * \value Image Any Image file. This includes both raster and vector formats
+ * \value Document Any file which counts as a document. Documents are generally
+ *  files which contain rich text, formatting and maybe images, but also just plain text
+ * \value Spreadsheet Any file which counts as a document. Documents are generally
+ * files which contain rich text, formatting and maybe images,
+ * but also just plain text.
+ * \value Presentation A Presentation file. This is a specialization of the Document type.
+ * Any file which has this type should also have the Document type
+ * \value Text Any file which contains text data (i.e. not binary data) counts as
+ * a Text file. As this also includes various file types which contain
+ * data collections (e.g. csv, json) or code (e.g. QML, Makefile),
+ * "Document" is typically the more appropriate type
+ * \value Folder A directory or folder
+ * \omitvalue LastType
  */
 enum Type {
     FirstType = 0,
     Empty = 0,
-
-    /**
-     * A file which contains a compressed collection of other files
-     * eg - tar, zip, rar, gz
-     * File types which use e.g. zip as an implementation detail -
-     * Java JAR files, OpenDocument formats, etc. - are not treated
-     * as Archive.
-     */
     Archive,
-
-    /**
-     * Used to mark any file which just contains audio. Do not use this
-     * type if the file also contains Video
-     */
     Audio,
-
-    /**
-     * Any file which contains Video. It may also contain Audio
-     */
     Video,
-
-    /**
-     * Any Image file. This includes both raster and vector formats.
-     */
     Image,
-
-    /**
-     * Any file which counts as a document. Documents are generally
-     * files which contain rich text, formatting and maybe images,
-     * but also just plain text.
-     */
     Document,
-
-    /**
-     * A SpreadSheet file. This is a specialization of the Document type
-     * Any file which has this type should also have the Document type
-     */
     Spreadsheet,
-
-    /**
-     * A Presentation file. This is a specialization of the Document type.
-     * Any file which has this type should also have the Document type
-     */
     Presentation,
-
-    /**
-     * Any file which contains text data (i.e. not binary data) counts as
-     * a Text file. As this also includes various file types which contain
-     * data collections (e.g. csv, json) or code (e.g. QML, Makefile),
-     * "Document" is typically the more appropriate type.
-     */
     Text,
-
-    /**
-     * A directory or folder
-     */
     Folder,
-
     LastType = Folder
 };
 

CMakeLists.txt
@@ -18,15 +18,12 @@ include(ECMAddTests)
 include(ECMGenerateExportHeader)
 include(ECMSetupVersion)
 include(ECMGenerateHeaders)
-include(ECMAddQch)
 include(ECMQtDeclareLoggingCategory)
 include(ECMDeprecationSettings)
+include(ECMGenerateQDoc)
 include(CheckStructHasMember)
 
 set(EXCLUDE_DEPRECATED_BEFORE_AND_AT 0 CACHE STRING "Control the range of deprecated API excluded from the build [default=0].")
-
-option(BUILD_QCH "Build API documentation in QCH format (for e.g. Qt Assistant, Qt Creator & KDevelop)" OFF)
-add_feature_info(QCH ${BUILD_QCH} "API documentation in QCH format (for e.g. Qt Assistant, Qt Creator & KDevelop)")
 
 set(kfilemetadata_version_header "${CMAKE_CURRENT_BINARY_DIR}/src/kfilemetadata_version.h")
 ecm_setup_version(PROJECT
@@ -140,16 +137,6 @@ add_subdirectory(tests)
 set(CMAKECONFIG_INSTALL_DIR "${KDE_INSTALL_CMAKEPACKAGEDIR}/KF6FileMetaData")
 
 ki18n_install(po)
-
-if (BUILD_QCH)
-    ecm_install_qch_export(
-        TARGETS KF6FileMetaData_QCH
-        FILE KF6FileMetaDataQchTargets.cmake
-        DESTINATION "${CMAKECONFIG_INSTALL_DIR}"
-        COMPONENT Devel
-    )
-    set(PACKAGE_INCLUDE_QCHTARGETS "include(\"\${CMAKE_CURRENT_LIST_DIR}/KF6FileMetaDataQchTargets.cmake\")")
-endif()
 
 include(CMakePackageConfigHelpers)
 

KF6FileMetaDataConfig.cmake.in
@@ -4,4 +4,3 @@ include(CMakeFindDependencyMacro)
 find_dependency(Qt6Core "@REQUIRED_QT_VERSION@")
 
 include("${CMAKE_CURRENT_LIST_DIR}/KF6FileMetaDataTargets.cmake")
-@PACKAGE_INCLUDE_QCHTARGETS@

docs/Doxyfile.local
@@ -1,10 +1,0 @@
-### KApiDox Project-specific Overrides File
-
-# define so that deprecated API is not skipped
-PREDEFINED += \
-    "KFILEMETADATA_ENABLE_DEPRECATED_SINCE(x, y)=1" \
-    "KFILEMETADATA_BUILD_DEPRECATED_SINCE(x, y)=1" \
-    "KFILEMETADATA_DEPRECATED_VERSION(x, y, t)=" \
-    "KFILEMETADATA_DEPRECATED_VERSION_BELATED(x, y, xt, yt, t)=" \
-    "KFILEMETADATA_ENUMERATOR_DEPRECATED_VERSION(x, y, t)=" \
-    "KFILEMETADATA_ENUMERATOR_DEPRECATED_VERSION_BELATED(x, y, xt, yt, t)="

src/CMakeLists.txt
@@ -81,6 +81,8 @@ ecm_generate_headers(KF6FileMetaData_CamelCase_HEADERS
     REQUIRED_HEADERS KF6FileMetaData_HEADERS
 )
 
+ecm_generate_qdoc(KF6FileMetaData kfilemetadata.qdocconf)
+
 install(TARGETS KF6FileMetaData EXPORT KF6FileMetaDataTargets ${KF_INSTALL_TARGETS_DEFAULT_ARGS})
 
 install(EXPORT KF6FileMetaDataTargets
@@ -100,34 +102,6 @@ install(FILES
 )
 
 configure_file(config-kfilemetadata.h.in ${CMAKE_CURRENT_BINARY_DIR}/config-kfilemetadata.h)
-
-if(BUILD_QCH)
-    ecm_add_qch(
-        KF6FileMetaData_QCH
-        NAME KFileMetaData
-        BASE_NAME KF6FileMetaData
-        VERSION ${KF_VERSION}
-        ORG_DOMAIN org.kde
-        SOURCES # using only public headers, to cover only public API
-            ${KF6FileMetaData_HEADERS}
-        MD_MAINPAGE "${CMAKE_SOURCE_DIR}/README.md"
-        LINK_QCHS
-            Qt6Core_QCH
-        INCLUDE_DIRS
-            ${CMAKE_CURRENT_BINARY_DIR}
-        BLANK_MACROS
-            KFILEMETADATA_EXPORT
-            KFILEMETADATA_DEPRECATED
-            KFILEMETADATA_DEPRECATED_EXPORT
-            "KFILEMETADATA_DEPRECATED_VERSION(x, y, t)"
-            "KFILEMETADATA_DEPRECATED_VERSION_BELATED(x, y, xt, yt, t)"
-            "KFILEMETADATA_ENUMERATOR_DEPRECATED_VERSION(x, y, t)"
-            "KFILEMETADATA_ENUMERATOR_DEPRECATED_VERSION_BELATED(x, y, xt, yt, t)"
-        TAGFILE_INSTALL_DESTINATION ${KDE_INSTALL_QTQCHDIR}
-        QCH_INSTALL_DESTINATION ${KDE_INSTALL_QTQCHDIR}
-        COMPONENT Devel
-    )
-endif()
 
 add_subdirectory(extractors)
 

src/embeddedimagedata.h
@@ -17,19 +17,46 @@
 
 namespace KFileMetaData {
 
-/**
- * \class EmbeddedImageData embeddedimagedata.h <KFileMetaData/EmbeddedImageData>
+// TODO KF6 make this an enum only in KF6 similar to properties.h
+/*!
+ * \class KFileMetaData::EmbeddedImageData
+ * \inheaderfile KFileMetaData/EmbeddedImageData
+ * \inmodule KFileMetaData
  *
  * \brief EmbeddedImageData defines enums for different image types that can
- * be extracted from the metadata of e.g.\ music files
- * @todo KF6 make this an enum only in KF6 similar to properties.h
+ * be extracted from the metadata of e.g. music files.
  */
 class KFILEMETADATA_EXPORT EmbeddedImageData {
 public:
+    /*!
+     *
+     */
     EmbeddedImageData();
     virtual ~EmbeddedImageData();
-    /**
-     * @see ImageTypes
+    /*!
+     * \value FrontCover
+     * \value Other
+     * \value FileIcon
+     * \value OtherFileIcon
+     * \value BackCover
+     * \value LeafletPage
+     * \value Media
+     * \value LeadArtist
+     * \value Artist
+     * \value Conductor
+     * \value Band
+     * \value Composer
+     * \value Lyricist
+     * \value RecordingLocation
+     * \value DuringRecording
+     * \value DuringPerformance
+     * \value MovieScreenCapture
+     * \value ColouredFish
+     * \value Illustration
+     * \value BandLogo
+     * \value PublisherLogo
+     * \value Unknown
+     * \value AllImages
      */
     enum ImageType {
         FrontCover = 1 << 0x0,
@@ -56,9 +83,6 @@ public:
         Unknown = 1 << 30,
         AllImages = 0x7fffffff
     };
-    /**
-     * Stores a combination of #ImageType values.
-     */
     Q_DECLARE_FLAGS(ImageTypes, ImageType)
 
 private:

src/extractionresult.h
@@ -19,8 +19,10 @@
 
 namespace KFileMetaData {
 class ExtractionResultPrivate;
-/**
- * \class ExtractionResult extractionresult.h <KFileMetaData/ExtractionResult>
+/*!
+ * \class KFileMetaData::ExtractionResult
+ * \inheaderfile KFileMetaData/ExtractionResult
+ * \inmodule KFileMetaData
  *
  * \brief The ExtractionResult class is where all the data extracted by
  * the indexer is saved.
@@ -30,29 +32,27 @@ class ExtractionResultPrivate;
  *
  * The derived class needs to implement 3 pure virtual functions through
  * which it receives the extracted data.
- *
- * \author Vishesh Handa <me@vhanda.in>
  */
 class KFILEMETADATA_EXPORT ExtractionResult
 {
 public:
-    /**
-     * @see Flags
+    /*!
+     * \value ExtractNothing
+     * \value ExtractMetaData
+     * \value ExtractPlainText
+     * \value[since 5.76] ExtractImageData
      */
     enum Flag {
         ExtractNothing = 0,
         ExtractMetaData = 1,
         ExtractPlainText = 2,
-        ExtractImageData = 4, ///< @since 5.76
+        ExtractImageData = 4,
     };
-    /**
-     * Stores a combination of #Flag values.
-     */
     Q_DECLARE_FLAGS(Flags, Flag)
 
-    /**
+    /*!
      * Create an ExtractionResult which can be passed be to Extractors. The
-     * extractors use the \p url, \p mimetype and \p flags in order to determine
+     * extractors use the \a url, \a mimetype and \a flags in order to determine
      * which file the data should be extracted from and which data should
      * be extracted.
      */
@@ -60,12 +60,12 @@ public:
     ExtractionResult(const ExtractionResult& rhs);
     virtual ~ExtractionResult();
 
-    /**
+    /*!
      * The input URL which the plugins will use to locate the file
      */
     QString inputUrl() const;
 
-    /**
+    /*!
      * The input MIME type. This MIME type should correspond with the
      * MIME types supported with the relevant plugin when it is being
      * passed to the Extractor, or be a subtype thereof.
@@ -75,33 +75,34 @@ public:
      */
     QString inputMimetype() const;
 
-    /**
+    /*!
      * The flags which the extraction plugin should considering following
      * when extracting metadata from the file
      */
     Flags inputFlags() const;
 
-    /**
+    /*!
      * This function is called by plugins when they wish for some plain
      * text to be indexed without any property. This generally corresponds
      * to the text content in a file
      */
     virtual void append(const QString& text) = 0;
 
-    /**
+    /*!
      * This function is called by the plugins when they wish to
      * add a key value pair which should be indexed. This function may be
      * called multiple times for the same key.
      *
-     * \p property This specifies a property name. It should be one of the
+     * \a property This specifies a property name. It should be one of the
      *             properties from the global list of properties.
      *
-     * \p value The value of the property
+     * \a value The value of the property
      */
     virtual void add(Property::Property property, const QVariant& value) = 0;
 
-    /**
+    /*!
      * This function is called by the plugins.
+     *
      * A type is a higher level classification of the file. A file can
      * have multiple types, but mostly when it does, those types are related.
      * Eg - Document and Presentation.
@@ -110,20 +111,21 @@ public:
      */
     virtual void addType(Type::Type type) = 0;
 
-    /**
+    /*!
      * This function is called by the plugins.
      *
-     * \p images The images to add
+     * \a images The images to add
+     *
      * \sa EmbeddedImageData
-     * @since 5.76
+     *
+     * \since 5.76
      */
     void addImageData(QMap<EmbeddedImageData::ImageType, QByteArray>&& images);
 
-    /**
+    /*!
      * Return embedded image data
      *
-     * \sa Flags::ExtractImageData
-     * @since 5.76
+     * \since 5.76
      */
     QMap<EmbeddedImageData::ImageType, QByteArray> imageData() const;
 

src/extractor.h
@@ -20,13 +20,19 @@ class ExtractorCollection;
 class ExtractorPlugin;
 class ExtractorPrivate;
 
-/**
- * \class Extractor extractor.h <KFileMetaData/Extractor>
+/*!
+ * \class KFileMetaData::Extractor
+ * \inheaderfile KFileMetaData/Extractor
+ * \inmodule KFileMetaData
  *
  * \brief The Extractor class is used to extract data from a file.
  */
 class KFILEMETADATA_EXPORT Extractor
 {
+    /*!
+     * \value AutoDeletePlugin
+     * \value DoNotDeletePlugin
+     */
     enum ExtractorPluginOwnership {
         AutoDeletePlugin,
         DoNotDeletePlugin,
@@ -36,8 +42,19 @@ public:
     Extractor(Extractor&&);
     virtual ~Extractor() noexcept;
 
+    /*!
+     *
+     */
     void extract(ExtractionResult* result);
+
+    /*!
+     *
+     */
     QStringList mimetypes() const;
+
+    /*!
+     *
+     */
     QVariantMap extractorProperties() const;
 
 private:

commit 2440d8ee5511df74b85ae631b4bee3c6e48b146d

Restore documentation for Spreadsheet document type
This was broken with commit 3224dd414ee8 ("Port API documentation
to QDoc syntax"), restore previous information.

src/types.h
@@ -43,9 +43,8 @@ namespace Type
  * \value Image Any Image file. This includes both raster and vector formats
  * \value Document Any file which counts as a document. Documents are generally
  *  files which contain rich text, formatting and maybe images, but also just plain text
- * \value Spreadsheet Any file which counts as a document. Documents are generally
- * files which contain rich text, formatting and maybe images,
- * but also just plain text.
+ * \value Spreadsheet A SpreadSheet file. This is a specialization of the Document type.
+ *  Any file which has this type should also have the Document type
  * \value Presentation A Presentation file. This is a specialization of the Document type.
  * Any file which has this type should also have the Document type
  * \value Text Any file which contains text data (i.e. not binary data) counts as