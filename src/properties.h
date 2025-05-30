17ce26903117906cbed9aec6e2b2851d18c56366
Kai Uwe Broulik <kde@privat.broulik.de>
2025-05-30 14:46:59 +0200

    Add AccessibleAltText property
    A brief textual description of the purpose and meaning of an image
    that can be accessed by assistive technology.
    
    This could for example be used by a social media app to automatically
    add an image description when posting a picture, if available.

--- src/properties.h
+++ src/properties.h
@@ -108,6 +108,7 @@ namespace Property {
  * \value[since 6.13] AudioCodec A string description of the codec used
  * \value[since 6.13] PixelFormat The Pixel format used by the media (image or video)
  * \value[since 6.13] ColorSpace The Color space used by the media (image or video)
+ * \value[since 6.16] AssistiveAlternateDescription A brief textual description of the purpose and meaning of an image. The text can be used by assistive technology.
  */
 enum Property {
     Empty = 0,
@@ -197,6 +198,7 @@ enum Property {
     AudioCodec,
     PixelFormat,
     ColorSpace,
+    AssistiveAlternateDescription,
 };
 
 } // namespace Property
