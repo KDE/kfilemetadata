0d63429c72a3839c36ec2134205977a44711b433
Stefan Brüns <stefan.bruens@rwth-aachen.de>
2025-07-06 16:53:14 +0200

    [PlaintextExtractor] Verify decoded text contains printable characters
    In case the decoded text mostly contains control characters and similar,
    but hardly any letters, number or punctuation, it is very likely the
    file actually contains fairly arbitrary binary data.
    
    This mostly happens when a file starts with a BOM, as it will be
    detected as text/plain by the mime database.
    
    BUG: 506570

--- src/extractors/plaintextextractor.cpp
+++ src/extractors/plaintextextractor.cpp
@@ -20,6 +20,34 @@
 
 using namespace KFileMetaData;
 
+namespace {
+
+bool looksLikeText(QStringView text)
+{
+    std::array<uint16_t, 7> groups{0};
+    for (const QChar c : text) {
+        if (auto category = c.category(); category > 29) {
+            continue;
+        } else if (category <= 2) { // Mark
+            groups[0]++;
+        } else if (category <= 5) { // Number
+            groups[1]++;
+        } else if (category <= 8) { // Separator
+            groups[2]++;
+        } else if (category <= 13) { // Other
+            groups[3]++;
+        } else if (category <= 18) { // Letter
+            groups[4]++;
+        } else if (category <= 25) { // Punctuation
+            groups[5]++;
+        } else if (category <= 29) { // Symbol
+            groups[6]++;
+        }
+    }
+    return (groups[0] + groups[3] + groups[6]) < (text.size() / 2);
+}
+} // namespace <anonymous>
+
 PlainTextExtractor::PlainTextExtractor(QObject* parent)
     : ExtractorPlugin(parent)
 {
@@ -98,6 +126,11 @@ void PlainTextExtractor::extract(ExtractionResult* result)
     QString text = codec.decode({chunk.data(), size});
     if (codec.hasError()) {
         qCDebug(KFILEMETADATA_LOG) << "Invalid" << codec.name() << "encoding. Ignoring" << result->inputUrl();
+        return;
+    }
+
+    // Sanity check
+    if (!looksLikeText(QStringView{text}.mid(0, 512))) {
         return;
     }
 
