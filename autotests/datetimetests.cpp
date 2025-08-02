e95ae95eb1b58e2645d15e9dde7ecb91f7c1bb08
Stefan Brüns <stefan.bruens@rwth-aachen.de>
2025-08-02 14:50:12 +0200

    [Exiv2Extractor] Add dedicated parser for Exif compound dates
    Exif stores the subsecond part of various datetimes in a separate field.
    
    Also, the timezone offset (available since Exif 2.32) is stored in a
    separate field. Older cameras may store the offset in a vendor specific
    field, typically as a UTC offset in minutes.

--- autotests/datetimetests.cpp
+++ autotests/datetimetests.cpp
@@ -10,6 +10,7 @@
 #include <QTimeZone>
 
 using namespace KFileMetaData;
+using namespace Qt::Literals::StringLiterals;
 
 class DateTimeTest : public QObject
 {
@@ -18,6 +19,8 @@ class DateTimeTest : public QObject
 private Q_SLOTS:
     void testParser();
     void testParser_data();
+    void testExifParser();
+    void testExifParser_data();
 };
 
 void DateTimeTest::testParser()
@@ -39,6 +42,36 @@ void DateTimeTest::testParser_data()
     QTest::addRow("ISO-8601-noTZ") << u"2025-07-27T14:35:56.448986511"_s       << QDateTime(QDate(2025, 07, 27), QTime(14, 35, 56, 449), QTimeZone::LocalTime);
     QTest::addRow("ISO-8601")      << u"2025-07-27T14:35:56.448986511+02:00"_s << QDateTime(QDate(2025, 07, 27), QTime(14, 35, 56, 449), QTimeZone(7200));
     QTest::addRow("ISO-date-only") << u"2025-07-27"_s                          << QDateTime(QDate(2025, 07, 27), QTime(0, 0, 0), QTimeZone::LocalTime);
+    // clang-format on
+}
+
+void DateTimeTest::testExifParser()
+{
+    QFETCH(QString, timeString);
+    QFETCH(QString, offsetString);
+    QFETCH(QString, subSecString);
+    QFETCH(QDateTime, expected);
+
+    auto dt = Parser::dateTimeFromExifString(timeString, subSecString, offsetString, {});
+
+    QCOMPARE(dt, expected);
+}
+
+void DateTimeTest::testExifParser_data()
+{
+    QTest::addColumn<QString>("timeString");
+    QTest::addColumn<QString>("subSecString");
+    QTest::addColumn<QString>("offsetString");
+    QTest::addColumn<QDateTime>("expected");
+
+    // clang-format off
+    QTest::addRow("noTZ")             << u"2025:07:27 14:35:56"_s << u""_s       << u""_s       << QDateTime(QDate(2025, 07, 27), QTime(14, 35, 56), QTimeZone::LocalTime);
+    QTest::addRow("ISO-TZ")           << u"2025:07:27 14:35:56"_s << u""_s       << u"+02:00"_s << QDateTime(QDate(2025, 07, 27), QTime(14, 35, 56), QTimeZone(7200));
+    QTest::addRow("Subsec-1")         << u"2025:07:27 14:35:56"_s << u"1"_s      << u""_s       << QDateTime(QDate(2025, 07, 27), QTime(14, 35, 56, 100), QTimeZone::LocalTime);
+    QTest::addRow("Subsec-200")       << u"2025:07:27 14:35:56"_s << u"200"_s    << u""_s       << QDateTime(QDate(2025, 07, 27), QTime(14, 35, 56, 200), QTimeZone::LocalTime);
+    QTest::addRow("Subsec-trim")      << u"2025:07:27 14:35:56"_s << u"34 "_s    << u""_s       << QDateTime(QDate(2025, 07, 27), QTime(14, 35, 56, 340), QTimeZone::LocalTime);
+    QTest::addRow("Subsec-rounddown") << u"2025:07:27 14:35:56"_s << u"34544 "_s << u""_s       << QDateTime(QDate(2025, 07, 27), QTime(14, 35, 56, 345), QTimeZone::LocalTime);
+    QTest::addRow("Subsec-roundup")   << u"2025:07:27 14:35:56"_s << u"34567"_s  << u""_s       << QDateTime(QDate(2025, 07, 27), QTime(14, 35, 56, 346), QTimeZone::LocalTime);
     // clang-format on
 }
 
